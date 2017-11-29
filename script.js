40bf850fa5d8ae0d94f192d44e44497ee32e7873
Kyle Arora <kylearora@gmail.com>
2017-11-29 13:02:04 -0600

    translated binary into DNA with bugs

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
  //method 1 - displays in browser
  function convert() {
-  var output = document.getElementById("binaryOutput");
+  var binOutput = ""
+  var binOutputArray = []
+  var dnaArray = []
   var input = document.getElementById("textInput").value;
+  var output = document.getElementById("binaryOutput");
   output.value = "";
   for (var i = 0; i < input.length; i++) {
-      output.value += "0" + input[i].charCodeAt(0).toString(2) + " ";
+      output.value += "0" + input[i].charCodeAt(0).toString(2);  
   }
+  var outputValue = output.value
+  console.log(outputValue);
+  binOutputArray = outputValue.match(/../g)
+  console.log(binOutputArray);
+  
+  for (var i = 0; i < binOutputArray.length; i++) {
+    if (binOutputArray[i] == "00") {
+      dnaArray.push("A")
+    } else if (binOutputArray[i] = "01") {
+      dnaArray.push("T")
+    } else if (binOutputArray[i] = "10") {
+      dnaArray.push("G")
+    } else if (binOutputArray[i] = "11") {
+      dnaArray.push("C")
+    }
+  }
+  console.log(dnaArray);
 }
+
+
+
+
+
 
 
 //method 2
-var output = []
+// var output = []
+// 
+// function textToBin(text) {
+//   var length = text.length
+//   for (var i = 0; i < length; i++) {
+//     var bin = text[i].charCodeAt().toString(2);
+//     output.push(Array(8-bin.length+1).join("0") + bin);
+//   } 
+//   return output;
+// }
+// 
+// cat = textToBin('cat')
+// console.log(cat)
+// console.log(output);
+// console.log(output.map(String))
+// 
 
-function textToBin(text) {
-  var length = text.length
-  for (var i = 0; i < length; i++) {
-    var bin = text[i].charCodeAt().toString(2);
-    output.push(Array(8-bin.length+1).join("0") + bin);
-  } 
-  return output.join(" ");
-}
-
-console.log(textToBin("test"));
-console.log(output);
 
 //split array into chunks
 
